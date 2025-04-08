3f726b55654fd34b451d6dd20e83e2d4732a0992
memefarmerr <memefarmerr@gmail.com>
2025-04-08 20:41:24 +1200

    updated for meme shop block

---
+++ src/main/java/com/memefarmerstudios/memecoins/memecoins.java
@@ -0,0 +1,96 @@
+package com.memefarmerstudios.memecoins;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+@Mod(memecoins.MODID)
+public class memecoins
+{
+    public static final String MODID = "memecoins";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(Registries.BLOCK, MODID);
+
+    public static final DeferredItem<Item> MEMECOIN = ITEMS.registerSimpleItem("memecoin", new Item.Properties().stacksTo(65));
+
+    public static final DeferredHolder<Block, Block> MEME_SHOP_BLOCK = BLOCKS.register("meme_shop_block", () -> new MemeShopBlock(BlockBehaviour.Properties.of().strength(2.5F)));
+
+    public static final DeferredItem<Item> MEME_SHOP_BLOCK_ITEM = ITEMS.register("meme_shop_block", () -> new BlockItem(MEME_SHOP_BLOCK.get(), new Item.Properties().stacksTo(64)));
+
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> CURRENCY_TAB = CREATIVE_MODE_TABS.register("currency", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.currency")) // Corrected the tab name to 'Currency'
+            .icon(() -> MEMECOIN.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
+                output.accept(MEMECOIN.get());
+                output.accept(MEME_SHOP_BLOCK_ITEM.get());
+            }).build());
+
+    public memecoins(IEventBus modEventBus, ModContainer modContainer)
+    {
+        modEventBus.addListener(this::commonSetup);
+
+        ITEMS.register(modEventBus);
+        CREATIVE_MODE_TABS.register(modEventBus);
+        BLOCKS.register(modEventBus);
+
+        NeoForge.EVENT_BUS.register(this);
+
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+        LOGGER.info("HELLO FROM COMMON SETUP");
+
+        if (Config.logDirtBlock)
+            LOGGER.info("DIRT BLOCK >> {}", BuiltInRegistries.BLOCK.getKey(Blocks.DIRT));
+
+        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
+
+        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+        LOGGER.info("HELLO from server starting");
+    }
+
+    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
+    }
+}
